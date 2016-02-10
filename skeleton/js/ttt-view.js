767d034de942813c21e7bdf512780f3bf5dc9a4f
App Academy Student <student@appacademy.io>
2016-02-10 14:57:38 -0800

    initial commit. develops CSS

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -1,4 +1,9 @@
 var View = function (game, $el) {
+  this.$el = $el;
+  this.game = game;
+
+  this.setupBoard();
+  this.bindEvents();
 };
 
 View.prototype.bindEvents = function () {
@@ -8,6 +13,22 @@ View.prototype.makeMove = function ($square) {
 };
 
 View.prototype.setupBoard = function () {
+  for (var i = 0; i < 3; i++) {
+    this.addRow();
+  }
+};
+
+View.prototype.addRow = function () {
+  // debugger;
+  var rowIdx = this.$el.find(".row").length;
+  var $row = $("<ul>").addClass(".row").addClass("group");
+
+  for (var colIdx = 0; colIdx < 3; colIdx++) {
+    var $square = $("<li>").addClass("square").data("pos", [rowIdx, colIdx]);
+    $row.append($square);
+  }
+
+  this.$el.append($row);
 };
 
 module.exports = View;
